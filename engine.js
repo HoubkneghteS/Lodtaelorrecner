c43ad7b48461bd87b97242edc8e66fd8da753b78
Adam Simons <houbkneghteS@users.noreply.github.com>
2017-12-17 20:19:03 -0600

    formatting document

--- engine.js
+++ engine.js
@@ -9,33 +9,35 @@ var dRate = 8, //deaths per thousand people per year
 
 //lang data:
 var en = {
-	"year":"Year",
-	"years":"Years",
-	"deaths":"Deaths",
-	"births":"Births",
-	"pop":"Population",
-	"deathrate":"Fatality Rate",
-	"birthrate":"Fertility Rate",
+	"year": "Year",
+	"years": "Years",
+	"deaths": "Deaths",
+	"births": "Births",
+	"pop": "Population",
+	"deathrate": "Fatality Rate",
+	"birthrate": "Fertility Rate",
 	"immigration": "Immigration",
-	"poppyramid":"Population Pyramid",
-	"stats":"Overall Stats",
-	"options":"Options",
-	"lang":"Language",
-	"credits":"by HoubkneghteS (Adam Simons)"};
+	"poppyramid": "Population Pyramid",
+	"stats": "Overall Stats",
+	"options": "Options",
+	"lang": "Language",
+	"credits": "by HoubkneghteS (Adam Simons)"
+};
 var de = {
-	"year":"Jahr",
-	"years":"Jahre",
-	"deaths":"Gestorbene",
-	"births":"Geborene",
-	"pop":"Bevölkerung",
-	"deathrate":"Sterberate",
-	"birthrate":"Geburtenrate",
+	"year": "Jahr",
+	"years": "Jahre",
+	"deaths": "Gestorbene",
+	"births": "Geborene",
+	"pop": "Bevölkerung",
+	"deathrate": "Sterberate",
+	"birthrate": "Geburtenrate",
 	"immigration": "Einwanderung",
-	"poppyramid":"Bevölkerungspyramide",
-	"stats":"Gesamtstastiken",
-	"options":"Optionen",
-	"lang":"Sprache",
-	"credits":"von HoubkneghteS (Adam Simons)"};
+	"poppyramid": "Bevölkerungspyramide",
+	"stats": "Gesamtstastiken",
+	"options": "Optionen",
+	"lang": "Sprache",
+	"credits": "von HoubkneghteS (Adam Simons)"
+};
 
 //population array -- year by year
 var pop = [
@@ -155,7 +157,7 @@ function widthHtml(id, width) {
 //sum for population
 function sum(start, end) {
 	var total = 0;
-	for (var i = start; i < end; i++){
+	for (var i = start; i < end; i++) {
 		total = total + pop[i];
 	}
 	return total;
@@ -185,31 +187,31 @@ function dChange(number) {
 function start(lang) {
 	if (lang == "en") r = en;
 	else r = de
-	
+
 	innerHtml("poppyramid", r.poppyramid);
 	innerHtml("stats", r.stats);
 	innerHtml("options", r.options);
-	
+
 	innerHtml("y1", `1 ${r.year}`);
 	innerHtml("y2", `2 ${r.years}`);
 	innerHtml("y5", `5 ${r.years}`);
-	
+
 	innerHtml("birthrate2", r.birthrate);
 	innerHtml("deathrate2", r.deathrate);
 	innerHtml("immigration2", r.immigration);
 	innerHtml("lang", r.lang);
-	
+
 	innerHtml("credits", r.credits);
-	
+
 	render(); //renders dynamic values
 }
 
 //rendering the data
 function render() {
 	//population tree
-	for(var i = 0; i < 100; i += 5) {
-		innerHtml(i , "<span id='plabel'>" + sum(i, i+4) + ` (${i}-${i+4})` + "</span>");
-		widthHtml(i, 1 + sum(i, i+4) / sum(0, pop.length) * 350);
+	for (var i = 0; i < 100; i += 5) {
+		innerHtml(i, "<span id='plabel'>" + sum(i, i + 4) + ` (${i}-${i + 4})` + "</span>");
+		widthHtml(i, 1 + sum(i, i + 4) / sum(0, pop.length) * 350);
 	}
 	innerHtml("100", "<span id='plabel'>" + pop[100] + " (100+)</span>");
 	widthHtml("100", 1 + pop[100] / sum(0, pop.length) * 350);
@@ -226,44 +228,44 @@ function render() {
 
 //simulates years
 function simulate(x) {
-	
+
 	for (var n = 0; n < x; n++) {
 		//ages population by 1 year
-		for(var i = 99; i >= 0; i--){
-			pop[i+1] += pop[i];
+		for (var i = 99; i >= 0; i--) {
+			pop[i + 1] += pop[i];
 			pop[i] = 0;
 		}
-		
+
 		var oldPop = sum(0, pop.length);
-		
+
 		//polynomial death rate equation
-		for(var i = 0; i <= 100; i++) {
-			pop[i] = Math.floor(pop[i] * (1 - dRate * (0.0000000000000015 *  Math.pow(i-18, 7) + dRate * 0.00007)));
+		for (var i = 0; i <= 100; i++) {
+			pop[i] = Math.floor(pop[i] * (1 - dRate * (0.0000000000000015 * Math.pow(i - 18, 7) + dRate * 0.00007)));
 		}
-		
+
 		deaths = oldPop - sum(0, pop.length); //calculates number of deaths
 		var deathPop = sum(0, pop.length);
-		
+
 		//simulates births
 		var mothers = 0;
-		
-		for(var i = lFertilityLimit; i <= uFertilityLimit; i++) {
-			mothers += pop[i]/2;
+
+		for (var i = lFertilityLimit; i <= uFertilityLimit; i++) {
+			mothers += pop[i] / 2;
 		}
-		
+
 		pop[0] += Math.ceil(mothers * bRate / (uFertilityLimit - lFertilityLimit)); //adds number to infant population
 		births = sum(0, pop.length) - deathPop; //calculates number of births
-		
+
 		//calculates immigration
-		for(var i = 20; i <= 40; i++) {
-			pop[i] += Math.round(immigration/20);
+		for (var i = 20; i <= 40; i++) {
+			pop[i] += Math.round(immigration / 20);
 		}
-		for(var i = 0; i <= 100; i++) {
+		for (var i = 0; i <= 100; i++) {
 			if (pop[i] < 0) pop[i] = 0; //extreme case fixer
 		}
-		
+
 		year++; //increments year
-		
+
 	}
 	render();
 }
